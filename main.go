49c8f8ebfe1258c7bd2036b62dad7837d710e85c
Conor Shortt <conorshortt123@gmail.com>
2024-01-07 21:41:54 +0000

    Scrape href links no threading

--- main.go
+++ main.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"fmt"
+	"log"
+
+	"github.com/gocolly/colly/v2"
+)
+
 func main() {
 	websites := []string{
 		"https://en.wikipedia.org/wiki/Main_Page",
@@ -13,8 +20,31 @@ func main() {
 		"https://stackoverflow.com/questions",
 		"https://www.cnn.com/",
 	}
+
+	// Loop over websites and scrape
+	for _, website := range websites {
+		scrape(website)
+	}
 }
 
-func addNums(a, b int) int {
-	return a + b
+func scrape(website string) {
+	// Create a new collector
+	c := colly.NewCollector()
+
+	// Set up a callback for when a visited HTML element is found
+	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+		link := e.Attr("href")
+		fmt.Println(link)
+	})
+
+	// Set up error handling
+	c.OnError(func(r *colly.Response, err error) {
+		log.Println("Request URL:", r.Request.URL, "failed with response:", r, "\nError:", err)
+	})
+
+	// Visit the initial URL
+	err := c.Visit(website)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
